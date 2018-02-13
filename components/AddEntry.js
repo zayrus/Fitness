e83c537f1a0934a486b58727df1561a3ef6440c8
Ernest <zayrus@gmail.com>
2018-02-13 14:12:58 -0300

    feat(logged screen): add screen when logged data is complete

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -4,7 +4,8 @@ import { getMetricMetaInfo, timeToString } from '../utils/helpers'
 import MySlider from './MySlider'
 import Stepper from './Stepper'
 import DateHeader from './DateHeader'
-
+import { Ionicons } from '@expo/vector-icons'
+import TextButton from './TextButton'
 
 function SubmitBtn ({ onPress }) {
     return (
@@ -68,9 +69,32 @@ export default class AddEntry extends Component {
         // Clear local notification
     }
 
+    reset = () => {
+        const key = timeToString()
+    
+        // Update Redux
+    
+        // Route to Home
+    
+        // Update "DB"
+    }
+
     render() {
         const metaInfo = getMetricMetaInfo()
-
+        if (this.props.alreadyLogged) {
+            return (
+              <View>
+                <Ionicons
+                  name={'ios-happy-outline'}
+                  size={100}
+                />
+                <Text>You already logged your information for today.</Text>
+                <TextButton onPress={this.reset}>
+                  Reset
+                </TextButton>
+              </View>
+            )
+        }
         return (
             <View>
                 <DateHeader date={(new Date()).toLocaleDateString()}/>
